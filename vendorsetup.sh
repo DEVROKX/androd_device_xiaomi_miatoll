e73af66be422b4aa99b6cdb7ea43fa3738dea6df
ULTIMATE-RITESH <ultimateritesh987@gmail.com>
2024-04-02 16:39:52 +0530

    miatoll: LMODroid Bringup

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/clarencekopitiam/vendor_xiaomi_miatoll.git -b 14-qpr2 vendor/xiaomi/miatoll
+git clone https://github.com/clarencelol/kernel_xiaomi_sm6250.git -b kinesis-v26 kernel/xiaomi/sm6250
+git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-21 hardware/sony/timekeep
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r510928.git -b 14.0 prebuilts/clang/host/linux-x86/clang-r510928
